1438cf2b9e8846b6be709d82e35c32ed3af713e2
DK <lzq@macbook-pro.local>
2016-03-06 13:31:38 +0800

    Initial Commit

---
+++ SearchHistoryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchHistoryDemo
+//
+//  Created by Lzq on 16/3/6.
+//  Copyright © 2016年 Lzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchHistoryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearchHistoryDemo
+//
+//  Created by Lzq on 16/3/6.
+//  Copyright © 2016年 Lzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
